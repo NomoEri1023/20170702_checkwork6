116ec4bd683149d6ef0d2e3f6aad278643aa9a01
ErikaNomoto <eripastanen1023@gmail.com>
2018-05-28 16:36:33 +0900

    Initial Commit

---
+++ Table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table
+//
+//  Created by 野本エリカ on 2018/05/28.
+//  Copyright © 2018年 Life is Tech!. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
